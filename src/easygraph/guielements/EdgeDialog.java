18c69c637cb030e784a8609dbf6a40f651ba1265
wengm17 <wengm17@bfh.ch>
2014-12-19 17:26:42 +0100

    AAdded newest journal entries Implement EdgeDialog and VertexDialog

--- src/easygraph/guielements/EdgeDialog.java
+++ src/easygraph/guielements/EdgeDialog.java
@@ -1,40 +1,57 @@
 package easygraph.guielements;
 
 import java.util.Optional;
-
+import javafx.geometry.Insets;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 public class EdgeDialog extends Dialog {
 
-	ButtonType buttonSave = new ButtonType("Speichern", ButtonData.OK_DONE);
-	ButtonType buttonCancle = new ButtonType("Abbrechen",
-			ButtonData.CANCEL_CLOSE);
+	final ButtonType buttonSave = new ButtonType("OK", ButtonData.OK_DONE);
+	final ButtonType buttonCancle = new ButtonType("Close", ButtonData.CANCEL_CLOSE);
+	final TextField weight = new TextField();
+	private GridPane grid = new GridPane();
 
 	public EdgeDialog() {
-		this.setTitle("dialog title");
-		this.setHeaderText("header text");
+		this.setTitle("Edit");
+		this.setHeaderText("Edit edge attributs");
+		this.setGraphic(new ImageView(new Image("file:resources/images/settings.png")));
 
-		this.setGraphic(new ImageView(new Image(
-				"file:resources/images/settings.png")));
-
+		// Set Dialog field background texts 
+		weight.setPromptText("weight");
+		// anzeige wert der original edge weight.setText(""); 
+				
+		// Create a custom dialog
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(0, 10, 0, 10));
+		
+		// Add and position text fields to the grid
+		grid.add(new Label("Weight:"), 0 , 0);
+		grid.add(weight, 1, 0);
+		
+		// Add grid to dialog
+		this.getDialogPane().setContent(grid);
+		
 		// Set the button types
-		this.getDialogPane().getButtonTypes().addAll(buttonSave, buttonCancle);
-
-		// dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL,
-		// ButtonType.APPLY);
-
+		this.getDialogPane().getButtonTypes().addAll(buttonSave, buttonCancle);	
 	}
 
 	public void showIt() {
 		Optional<ButtonType> result = this.showAndWait();
 		if (result.get().getButtonData() == ButtonData.OK_DONE) {
-			// update
+			// to do update edge value 
+			
+			// Check if user changed attribute
+			if(!this.weight.getText().equals("")) {
+				System.out.println("Changed edge weight to: " + this.weight.getText());
+			}
 		}
-
 	}
-
 }
--- src/easygraph/guielements/VertexDialog.java
+++ src/easygraph/guielements/VertexDialog.java
@@ -1,40 +1,57 @@
 package easygraph.guielements;
 
 import java.util.Optional;
-
+import javafx.geometry.Insets;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
 
 public class VertexDialog extends Dialog {
 
-	ButtonType buttonSave = new ButtonType("Speichern", ButtonData.OK_DONE);
-	ButtonType buttonCancle = new ButtonType("Abbrechen",
-			ButtonData.CANCEL_CLOSE);
-
+	final ButtonType buttonSave = new ButtonType("Ok", ButtonData.OK_DONE);
+	final ButtonType buttonCancle = new ButtonType("Close", ButtonData.CANCEL_CLOSE);
+	final TextField name = new TextField();
+	private GridPane grid = new GridPane();
+	
 	public VertexDialog() {
-		this.setTitle("dialog title");
-		this.setHeaderText("header text");
-
-		this.setGraphic(new ImageView(new Image(
-				"file:resources/images/settings.png")));
-
+		this.setTitle("Edit");
+		this.setHeaderText("Edit vertex attributes");
+		this.setGraphic(new ImageView(new Image("file:resources/images/settings.png")));
+		
+		// Set Dialog field background texts 
+		name.setPromptText("name");
+				
+		// Create a custom dialog
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(0, 10, 0, 10));
+		
+		// Add and position text fields to the grid
+		grid.add(new Label("Name:"), 0 , 0);
+		grid.add(name, 1, 0);
+		
+		// Add grid to dialog
+		this.getDialogPane().setContent(grid);
+		
 		// Set the button types
-		this.getDialogPane().getButtonTypes().addAll(buttonSave, buttonCancle);
-
-		// dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL,
-		// ButtonType.APPLY);
-
+		this.getDialogPane().getButtonTypes().addAll(buttonSave, buttonCancle);		
 	}
 
 	public void showIt() {
 		Optional<ButtonType> result = this.showAndWait();
 		if (result.get().getButtonData() == ButtonData.OK_DONE) {
-			// update
+			// to do update vertex value
+			
+			// Check if user changed attribute
+			if(!this.name.getText().equals("")) {
+				System.out.println("Changed vertex name to: " + this.name.getText());
+			}
 		}
-
 	}
-
 }
